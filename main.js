f840d46bb975e9df7a67bcffd4aa0aa9a25204f3
veronicasilva20 <santinoveronicasilva@gmail.com>
2023-05-07 11:59:22 -0300

    agrego texto script

---
+++ main.js
@@ -0,0 +1,15 @@
+alert ("mi publicidad");
+const contenido= document.getElementById("contenido");
+let nombre ="aceite cocicnero";
+let precio="$25";
+
+const COLOR_ROJO ="#ff0000";
+
+let resultado ="nombre:" + nombre;
+
+console.log(nombre);
+
+nombre="Aceitye cocinero";
+
+
+
